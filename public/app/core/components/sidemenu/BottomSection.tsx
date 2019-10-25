e4d16bc03c95e16a649eecd5471b34e264244e37
Miek Gieben <miek@miek.nl>
2019-10-25 20:14:15 +0100

    Remove signout button
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- public/app/core/components/sidemenu/BottomSection.tsx
+++ public/app/core/components/sidemenu/BottomSection.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import _ from 'lodash';
-import SignIn from './SignIn';
 import BottomNavLinks from './BottomNavLinks';
 import { contextSrv } from 'app/core/services/context_srv';
 import config from '../../config';
@@ -9,7 +8,6 @@ import { NavModelItem } from '@grafana/data';
 export default function BottomSection() {
   const navTree: NavModelItem[] = _.cloneDeep(config.bootData.navTree);
   const bottomNav: NavModelItem[] = _.filter(navTree, item => item.hideFromMenu);
-  const isSignedIn = contextSrv.isSignedIn;
   const user = contextSrv.user;
 
   if (user && user.orgCount > 1) {
@@ -21,7 +19,6 @@ export default function BottomSection() {
 
   return (
     <div className="sidemenu__bottom">
-      {!isSignedIn && <SignIn />}
       {bottomNav.map((link, index) => {
         return <BottomNavLinks link={link} user={user} key={`${link.url}-${index}`} />;
       })}
